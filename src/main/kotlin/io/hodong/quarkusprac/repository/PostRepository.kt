807576e25d0e8572857f3ce81772c0b2649bf740
hodongman <jhd9206@gmail.com>
2022-02-09 01:51:37 +0900

    post crud update

--- src/main/kotlin/io/hodong/quarkusprac/repository/PostRepository.kt
+++ src/main/kotlin/io/hodong/quarkusprac/repository/PostRepository.kt
@@ -38,19 +38,29 @@ class PostRepository(val client: MySQLPool) {
                 .execute(Tuple.of(id))
     }
     */
-    /*
+    
     fun save(post: PostModel): Uni<UUID> {
         return this.client
                 .preparedQuery("INSERT INTO posts (title, content) VALUES ($1, $2) RETURNING (id)")
                 .execute(Tuple.of(post.title, post.content))
+
                 .onItem().transformToUni {
-                    row -> Uni.createFrom().item(row)                
+                    row -> Uni.createFrom().item(row.iterator().next().getUUID("id"))
                 }
+                //.map {
+                //    iter -> iter.iterator().next().getUUID("id")
+                //}
+    }
+
+    fun update(id: UUID, post: PostModel): Uni<Int> {
+        return this.client
+                .preparedQuery("UPDATE posts SET title=$1, content=$2 WHERE id=$3")
+                .execute(Tuple.of(post.title, post.content, id))
                 .map {
-                    it -> it.next().getUUID("id")
+                    it -> it.count()
                 }
     }
-    */
+    
     fun delete(id: UUID): Uni<Int> {
         return client.preparedQuery("DELETE FROM posts WHERE id=$1")
                 .execute(Tuple.of(id))
